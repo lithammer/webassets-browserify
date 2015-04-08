228d285c2b34cfe1edda8d8028a6d6a4fa329a04
Peter Renström <renstrom.peter@gmail.com>
2015-04-08 11:44:09 +0200

    Add docstring.

--- webassets_browserify/__init__.py
+++ webassets_browserify/__init__.py
@@ -4,6 +4,27 @@ __all__ = ['Browserify']
 
 
 class Browserify(ExternalTool):
+    """Use Browserify to bundle assets.
+
+    Requires the Browserify executable to be available externally. You can
+    install it using `Node Package Manager <http://npmjs.org/>`_::
+
+        $ npm install -g browserify
+
+    Supported configuration options:
+
+    BROWSERIFY_BIN
+        The path to the Browserify binary. If not set, assumes ``browserify``
+        is in the system path.
+
+    BROWSERIFY_TRANSFORMS
+        A list of Browserify transforms to use. Each transform will be included
+        via Browserify's command-line ``--transform`` argument.
+
+    BROWSERIFY_EXTRA_ARGS
+        A list of any additional command-line arguments.
+
+    """
 
     name = 'browserify'
     max_debug_level = None
