81a0ffba0a744df61da246be5a1729981c2a32b4
Peter Renström <renstrom.peter@gmail.com>
2015-07-28 09:25:35 +0200

    Bump version (1.0.2 → 1.1.0).

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='webassets-browserify',
-      version='1.0.2',
+      version='1.1.0',
       description='Browserify filter for webassets',
       long_description=__doc__,
       author='Peter Renström',
